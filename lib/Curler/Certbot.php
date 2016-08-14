22fa4c087a3e837be18d1df68340d3bb29092a1d
metaclassing <github@secureobscure.com>
2016-08-14 11:01:42 -0400

    Applied fixes from StyleCI

--- lib/Curler/Certbot.php
+++ lib/Curler/Certbot.php
@@ -1,114 +1,117 @@
 <?php
+
 namespace metaclassing\Curler;
 
 class Certbot extends Curler
 {
-	public $token;
-	public $baseurl;
-	public $accounttypes = ['acme', 'ca'];
-	public $accounts = [];
+    public $token;
+    public $baseurl;
+    public $accounttypes = ['acme', 'ca'];
+    public $accounts = [];
 
-	public function __construct($baseurl, $clientcert)
-	{
-		$this->baseurl = $baseurl;
+    public function __construct($baseurl, $clientcert)
+    {
+        $this->baseurl = $baseurl;
 
-		$this->curl = curl_init();
-		curl_setopt($this->curl, CURLOPT_HEADER         , false);
-		curl_setopt($this->curl, CURLOPT_SSLCERT        , $clientcert);
-		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER , true);
+        $this->curl = curl_init();
+        curl_setopt($this->curl, CURLOPT_HEADER, false);
+        curl_setopt($this->curl, CURLOPT_SSLCERT, $clientcert);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
 //		curl_setopt($this->curl, CURLOPT_VERBOSE      , true);
 
-		// Get the list of accounts we can access and certs in each
-		$this->authenticate();
-		$this->listAccounts();
-		$this->listCertificates();
-	}
+        // Get the list of accounts we can access and certs in each
+        $this->authenticate();
+        $this->listAccounts();
+        $this->listCertificates();
+    }
 
-	public function getjson($url, $referer = "")
-	{
-		if ($this->token) {
-			$url .= '?token=' . $this->token;
-		}
-		curl_setopt($this->curl, CURLOPT_URL            , $url);
-		curl_setopt($this->curl, CURLOPT_REFERER        , $referer);
+    public function getjson($url, $referer = '')
+    {
+        if ($this->token) {
+            $url .= '?token='.$this->token;
+        }
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_REFERER, $referer);
 
-		$response = $this->curl_exec();
-		if (! \metaclassing\Utility::isJson($response)) {
-			throw new \Exception('Did not get JSON response from web service, got ' . $response);
-		}
+        $response = $this->curl_exec();
+        if (!\metaclassing\Utility::isJson($response)) {
+            throw new \Exception('Did not get JSON response from web service, got '.$response);
+        }
 
-		$response = \metaclassing\Utility::decodeJson($response);
-		if ( isset($response['status_code']) && $response['status_code'] != 200) {
-			throw new \Exception('Did not get a 200 response from web service for last call,' .
-								 ' url was ' . $url . ' response was ' . \metaclassing\Utility::dumperToString($response));
-		}
+        $response = \metaclassing\Utility::decodeJson($response);
+        if (isset($response['status_code']) && $response['status_code'] != 200) {
+            throw new \Exception('Did not get a 200 response from web service for last call,'.
+                                 ' url was '.$url.' response was '.\metaclassing\Utility::dumperToString($response));
+        }
 
-		return $response;
-	}
+        return $response;
+    }
 
-	public function authenticate()
-	{
-		// get our json web token for authentication
-		$url = $this->baseurl . 'authenticate';
-		$result = $this->getjson($url);
-		if (!$result['token']) {
-			throw new \Exception('Could not authenticate to web service and get JSON web token');
-		}
-		$this->token = $result['token'];
-	}
+    public function authenticate()
+    {
+        // get our json web token for authentication
+        $url = $this->baseurl.'authenticate';
+        $result = $this->getjson($url);
+        if (!$result['token']) {
+            throw new \Exception('Could not authenticate to web service and get JSON web token');
+        }
+        $this->token = $result['token'];
+    }
 
-	public function listAccounts()
-	{
-		foreach($this->accounttypes as $type) {
-			$url = $this->baseurl . $type . '/account';
-			$response = $this->getjson($url);
-			$accounts = $response['accounts'];
-			foreach($accounts as $account) {
-				$this->accounts[$type][$account['id']] = $account;
-			}
-		}
-		return $this->accounts;
-	}
+    public function listAccounts()
+    {
+        foreach ($this->accounttypes as $type) {
+            $url = $this->baseurl.$type.'/account';
+            $response = $this->getjson($url);
+            $accounts = $response['accounts'];
+            foreach ($accounts as $account) {
+                $this->accounts[$type][$account['id']] = $account;
+            }
+        }
 
-	public function listCertificates()
-	{
-		foreach($this->accounts as $type => $accounts) {
-			foreach($accounts as $index => $account) {
-				$url = $this->baseurl . $type . '/account/' . $account['id'] . '/certificate';
-				$response = $this->getjson($url);
-				$certificates = $response['certificates'];
-				foreach($certificates as $certificate) {
-					$this->accounts[$type][$index]['certificates'][$certificate['id']] = $certificate;
-				}
-			}
-		}
-		return $this->accounts;
-	}
+        return $this->accounts;
+    }
 
-	// Really only necessary until I implement a direct API route to search for certificates...
-	public function findCertificate($name)
-	{
-		foreach($this->accounttypes as $type) {
-			foreach($this->accounts[$type] as $account) {
-				foreach($account['certificates'] as $certificate) {
-					if ($certificate['name'] == $name) {
-						$certificate['accounttype'] = $type;
-						$certificate['pemurl'] = $this->baseurl . $type . '/account/' . $certificate['account_id'] . '/certificate/' . $certificate['id'] . '/pem';
-						return $certificate;
-					}
-				}
-			}
-		}
-	}
+    public function listCertificates()
+    {
+        foreach ($this->accounts as $type => $accounts) {
+            foreach ($accounts as $index => $account) {
+                $url = $this->baseurl.$type.'/account/'.$account['id'].'/certificate';
+                $response = $this->getjson($url);
+                $certificates = $response['certificates'];
+                foreach ($certificates as $certificate) {
+                    $this->accounts[$type][$index]['certificates'][$certificate['id']] = $certificate;
+                }
+            }
+        }
 
-	public function getCertificate($name)
-	{
-		$certificate = $this->findCertificate($name);
-		if (!$certificate) {
-			throw new \Exception('Could not find a certificate with the name of ' . $name);
-		}
+        return $this->accounts;
+    }
 
-		return $this->get($certificate['pemurl'] . '?token=' . $this->token);
-	}
+    // Really only necessary until I implement a direct API route to search for certificates...
+    public function findCertificate($name)
+    {
+        foreach ($this->accounttypes as $type) {
+            foreach ($this->accounts[$type] as $account) {
+                foreach ($account['certificates'] as $certificate) {
+                    if ($certificate['name'] == $name) {
+                        $certificate['accounttype'] = $type;
+                        $certificate['pemurl'] = $this->baseurl.$type.'/account/'.$certificate['account_id'].'/certificate/'.$certificate['id'].'/pem';
 
+                        return $certificate;
+                    }
+                }
+            }
+        }
+    }
+
+    public function getCertificate($name)
+    {
+        $certificate = $this->findCertificate($name);
+        if (!$certificate) {
+            throw new \Exception('Could not find a certificate with the name of '.$name);
+        }
+
+        return $this->get($certificate['pemurl'].'?token='.$this->token);
+    }
 }
--- lib/Curler/Curler.php
+++ lib/Curler/Curler.php
@@ -1,105 +1,118 @@
 <?php
+
 namespace metaclassing\Curler;
 
 class Curler
 {
-	public $curl;
+    public $curl;
 
     // constructor sets mandatory fields
     public function __construct($cookiePath = '/tmp/curler.cookiejar')
     {
         // set our cookie path
-        $this->cookiePath= $cookiePath;
+        $this->cookiePath = $cookiePath;
 
         // setup curl handle
         $this->curl = curl_init();
-        curl_setopt($this->curl, CURLOPT_HEADER         , false);
-        curl_setopt($this->curl, CURLOPT_NOBODY         , false);
+        curl_setopt($this->curl, CURLOPT_HEADER, false);
+        curl_setopt($this->curl, CURLOPT_NOBODY, false);
         // set curl cookie options
-        curl_setopt($this->curl, CURLOPT_COOKIEJAR      , $this->cookiePath);
-        curl_setopt($this->curl, CURLOPT_COOKIEFILE     , $this->cookiePath);
+        curl_setopt($this->curl, CURLOPT_COOKIEJAR, $this->cookiePath);
+        curl_setopt($this->curl, CURLOPT_COOKIEFILE, $this->cookiePath);
         // set curl user agent spoof
-        curl_setopt($this->curl, CURLOPT_USERAGENT      , "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
-        curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST , false);
-        curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER , false);
-        curl_setopt($this->curl, CURLOPT_CERTINFO		, true);
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER , true);
-        curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION , true);
+        curl_setopt($this->curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7');
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($this->curl, CURLOPT_CERTINFO, true);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, true);
         //curl_setopt($this->curl, CURLOPT_VERBOSE      , true);
     }
 
-	public function get($url, $referer = "")
-	{
-		curl_setopt($this->curl, CURLOPT_URL            , $url);
-		curl_setopt($this->curl, CURLOPT_REFERER        , $referer);
-		return $this->curl_exec();
-	}
+    public function get($url, $referer = '')
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_REFERER, $referer);
 
-	public function post($url, $referer = "", $post = "")
-	{
-		curl_setopt($this->curl, CURLOPT_URL            , $url);
-		curl_setopt($this->curl, CURLOPT_REFERER        , $url);
-		curl_setopt($this->curl, CURLOPT_POST           , true);
-		curl_setopt($this->curl, CURLOPT_POSTFIELDS     , $post);
-		return $this->curl_exec();
-	}
+        return $this->curl_exec();
+    }
 
-	// curl wrapper for fun
-	public function curl_getinfo() {
-		return curl_getinfo($this->curl);
-	}
+    public function post($url, $referer = '', $post = '')
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_REFERER, $url);
+        curl_setopt($this->curl, CURLOPT_POST, true);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $post);
 
-	public function curl_exec() {
-		return curl_exec($this->curl);
-	}
+        return $this->curl_exec();
+    }
 
-	/** The same as curl_exec except tries its best to convert the output to utf8 **/
-	public function curl_exec_utf8() {
-	    $data = curl_exec($this->curl);
-	    if (!is_string($data)) return $data;
+    // curl wrapper for fun
+    public function curl_getinfo()
+    {
+        return curl_getinfo($this->curl);
+    }
 
-	    unset($charset);
-	    $content_type = curl_getinfo($this->curl, CURLINFO_CONTENT_TYPE);
+    public function curl_exec()
+    {
+        return curl_exec($this->curl);
+    }
 
-	    /* 1: HTTP Content-Type: header */
-	    preg_match( '@([\w/+]+)(;\s*charset=(\S+))?@i', $content_type, $matches );
-	    if ( isset( $matches[3] ) )
-	        $charset = $matches[3];
+    /** The same as curl_exec except tries its best to convert the output to utf8 **/
+    public function curl_exec_utf8()
+    {
+        $data = curl_exec($this->curl);
+        if (!is_string($data)) {
+            return $data;
+        }
 
-	    /* 2: <meta> element in the page */
-	    if (!isset($charset)) {
-	        preg_match( '@<meta\s+http-equiv="Content-Type"\s+content="([\w/]+)(;\s*charset=([^\s"]+))?@i', $data, $matches );
-	        if ( isset( $matches[3] ) )
-	            $charset = $matches[3];
-	    }
+        unset($charset);
+        $content_type = curl_getinfo($this->curl, CURLINFO_CONTENT_TYPE);
 
-	    /* 3: <xml> element in the page */
-	    if (!isset($charset)) {
-	        preg_match( '@<\?xml.+encoding="([^\s"]+)@si', $data, $matches );
-	        if ( isset( $matches[1] ) )
-	            $charset = $matches[1];
-	    }
+        /* 1: HTTP Content-Type: header */
+        preg_match('@([\w/+]+)(;\s*charset=(\S+))?@i', $content_type, $matches);
+        if (isset($matches[3])) {
+            $charset = $matches[3];
+        }
 
-	    /* 4: PHP's heuristic detection */
-	    if (!isset($charset)) {
-	        $encoding = mb_detect_encoding($data);
-	        if ($encoding)
-	            $charset = $encoding;
-	    }
+        /* 2: <meta> element in the page */
+        if (!isset($charset)) {
+            preg_match('@<meta\s+http-equiv="Content-Type"\s+content="([\w/]+)(;\s*charset=([^\s"]+))?@i', $data, $matches);
+            if (isset($matches[3])) {
+                $charset = $matches[3];
+            }
+        }
 
-	    /* 5: Default for HTML */
-	    if (!isset($charset)) {
-	        if (strstr($content_type, "text/html") === 0)
-	            $charset = "ISO 8859-1";
-	    }
+        /* 3: <xml> element in the page */
+        if (!isset($charset)) {
+            preg_match('@<\?xml.+encoding="([^\s"]+)@si', $data, $matches);
+            if (isset($matches[1])) {
+                $charset = $matches[1];
+            }
+        }
 
-	    /* Convert it if it is anything but UTF-8 */
-	    /* You can change "UTF-8"  to "UTF-8//IGNORE" to
-	       ignore conversion errors and still output something reasonable */
-	   	 if (isset($charset) && strtoupper($charset) != "UTF-8")
-	        $data = iconv($charset, 'UTF-8', $data);
+        /* 4: PHP's heuristic detection */
+        if (!isset($charset)) {
+            $encoding = mb_detect_encoding($data);
+            if ($encoding) {
+                $charset = $encoding;
+            }
+        }
 
-	    return $data;
-	}
+        /* 5: Default for HTML */
+        if (!isset($charset)) {
+            if (strstr($content_type, 'text/html') === 0) {
+                $charset = 'ISO 8859-1';
+            }
+        }
 
+        /* Convert it if it is anything but UTF-8 */
+        /* You can change "UTF-8"  to "UTF-8//IGNORE" to
+           ignore conversion errors and still output something reasonable */
+         if (isset($charset) && strtoupper($charset) != 'UTF-8') {
+             $data = iconv($charset, 'UTF-8', $data);
+         }
+
+        return $data;
+    }
 }
